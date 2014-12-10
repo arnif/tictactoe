051806a9d3e0a4ad9258a502a6584c3fa3375b26
arnif <arnifa@gmail.com>
2014-12-10 16:33:47 +0000

    e2e can play another game of tictactoe

--- e2e/tictactoe/tictactoe.spec.js
+++ e2e/tictactoe/tictactoe.spec.js
@@ -91,7 +91,7 @@ describe('TicTacToe Create View', function() {
 
   });
 
-  it('should play a game of TicTacToe', function() {
+  it('should play a game of TicTacToe and X will win (row)', function() {
 
     game.nameOfGame("FirstGame");
     game.nameOfUser("Bruce");
@@ -152,7 +152,92 @@ describe('TicTacToe Create View', function() {
 
 
           });
-        //game.expectGameBoardShowing();
+
+        // closes the current window
+        browser.executeScript('window.close()');
+
+      });
+
+
+    });
+
+
+
+  });
+
+  it('should play a game of TicTacToe and end with draw', function() {
+
+    game.nameOfGame("FirstGame");
+    game.nameOfUser("Bruce");
+    game.createGame();
+    game.waitForTictactoePage();
+
+    browser.getCurrentUrl().then(function(url) {
+
+      browser.getAllWindowHandles().then(function (handles) {
+
+        // handle of first window
+        var originalHandle = handles[0];
+
+        // open new window
+        browser.executeScript('window.open("'+ url +'", "second-window")');
+
+        // switch to new window
+        browser.switchTo().window('second-window');
+
+        // do something within context of new window
+        game.nameOfJoinUser('Clark');
+        game.joinGame();
+        game.waitForTictactoePage();
+
+
+        // switch to original window
+        browser.switchTo().window(originalHandle);
+
+        // do something within context of original window
+
+        browser.driver.wait(function(){
+          return    browser.driver.isElementPresent(by.css('#tictactoeBoard')).then(function(el){
+            return el === true;
+          });
+        }).
+          then(function(){
+            game.expectEnjoyTheGameMessage();
+            game.expectGameBoardShowing();
+            game.waitForTictactoePage();
+
+            play.x0y0('X'); //1
+
+            browser.switchTo().window('second-window');
+            play.x0y1('O'); //2
+
+            browser.switchTo().window(originalHandle);
+            play.x0y2('X'); //3
+
+            browser.switchTo().window('second-window');
+            play.x1y0('O'); //4
+
+            browser.switchTo().window(originalHandle);
+            play.x1y2('X'); //5
+
+            browser.switchTo().window('second-window');
+            play.x1y1('O'); //6
+
+            browser.switchTo().window(originalHandle);
+            play.x2y1('X'); // 7
+
+            browser.switchTo().window('second-window');
+            play.x2y2('O'); //8
+
+            browser.switchTo().window(originalHandle);
+            play.x2y0('X'); // 9
+
+            game.waitForTictactoePage();
+
+            play.expectGameMessage('Game OVER! - Winner: Draw!');
+
+
+          });
 
         // closes the current window
         browser.executeScript('window.close()');
