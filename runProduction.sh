abb95d540a4d5635473c1f195c887eecf567d8fa
arnif <arnifa@gmail.com>
2014-12-11 14:55:54 +0000

    fix script

--- runProduction.sh
+++ runProduction.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo "Killing all docker process"
 docker kill $(docker ps -a -q)
+echo "pulling tictactoe"
 docker pull arnif/tictactoe
+echo "Running TicTacToe"
 docker run -p 80:8080 -d -e "NODE_ENV=production" arnif/tictactoe
