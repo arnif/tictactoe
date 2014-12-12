db56219ecac7e26c08f8192e7c9da3841195b146
arnif <arnifa@gmail.com>
2014-12-12 14:34:59 +0000

    acceptance tests should run on test database

--- runAcceptance.sh
+++ runAcceptance.sh
@@ -4,4 +4,4 @@ docker kill $(docker ps -a -q)
 echo "pulling tictactoe"
 docker pull arnif/tictactoe
 echo "Running TicTacToe"
-docker run -p 80:8080 -d -e "NODE_ENV=test" arnif/tictactoe
+docker run -p 80:8080 -d -e "NODE_ENV=acceptance" arnif/tictactoe
