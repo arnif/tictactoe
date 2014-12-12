3a5228cf5043362ca690e66174aae87717fa9a43
arnif <arnifa@gmail.com>
2014-12-12 14:48:10 +0000

    acceptance tests should run on test database

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,6 +5,6 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://test:test@ds063630.mongolab.com:63630/tictac-test'
+    uri: 'mongodb://test:test@ds063630.mongolab.com:63630/tictac-dev'
   }
 };
