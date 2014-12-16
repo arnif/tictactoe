6aea08c2eeb61a35bf00a2c16b663bce96f4d693
arnif <arnifa@gmail.com>
2014-12-16 20:13:36 +0000

    removing un used scripts

--- runProduction.sh
+++ runProduction.sh
@@ -6,8 +6,8 @@ docker kill $(docker ps -a -q)
 echo "pulling tictactoe"
 docker pull arnif/tictactoe
 
-# mongoose-migrate ekki attatchment
 echo "Running migrations"
 docker run -e "NODE_ENV=production" arnif/tictactoe ./node_modules/.bin/mongoose-migrate
+
 echo "Running TicTacToe"
 docker run -p 80:8080 -d -e "NODE_ENV=production" arnif/tictactoe
