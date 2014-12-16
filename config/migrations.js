2edf24f8e793174434e546daf2e5ae5d08194c73
arnif <arnifa@gmail.com>
2014-12-16 12:13:21 +0000

    migrations test nr 5

--- config/migrations.js
+++ config/migrations.js
@@ -4,5 +4,10 @@ module.exports = {
     schema: { 'migration': {} },
     modelName: 'Migration',
     db: process.env.MONGOHQ_URL || 'mongodb://development:development@ds063630.mongolab.com:63630/tictac-dev'
+  },
+  production: {
+    schema: { 'migration': {} },
+    modelName: 'Migration',
+    db: process.env.MONGOHQ_URL || 'mongodb://production:production@ds063630.mongolab.com:63630/tictac'
   }
 };
--- runProduction.sh
+++ runProduction.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+set -e
+
 echo "Killing all docker process"
 docker kill $(docker ps -a -q)
 echo "pulling tictactoe"
 docker pull arnif/tictactoe
 
 # mongoose-migrate ekki attatchment
+echo "Running migrations"
+docker run -d "NODE_ENV=production" arnif/tictactoe migrate
 echo "Running TicTacToe"
 docker run -p 80:8080 -d -e "NODE_ENV=production" arnif/tictactoe
