81d4b7642d55ef0bb624f2db5d4e85627cba5d72
arnif <arnifa@gmail.com>
2014-12-16 11:53:02 +0000

    migrations test nr 3

--- config/migrations.js
+++ config/migrations.js
@@ -3,6 +3,6 @@ module.exports = {
   development: {
     schema: { 'migration': {} },
     modelName: 'Migration',
-    db: process.env.MONGOHQ_URL || 'mongodb://localhost/app_development'
+    db: process.env.MONGOHQ_URL || 'mongodb://development:development@ds063630.mongolab.com:63630/tictac-dev'
   }
 };
